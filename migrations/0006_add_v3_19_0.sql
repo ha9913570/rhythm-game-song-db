5c595af919968e5ea8d576e5e53dcb325ec8da04
haruto356 <harutoharu327@gmail.com>
2026-04-12 20:39:44 +0900

    add: v3.19.0のデータを追加

---
+++ migrations/0006_add_v3_19_0.sql
@@ -0,0 +1,3 @@
+INSERT INTO Composer (ComposerName) VALUES ("Daily天利");
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("After ZABANIYA (MUG Edit)", 250, 222, 142, "3.19.0", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (301, 5, 10, 15, NULL, 364, 723, 1130, NULL);
