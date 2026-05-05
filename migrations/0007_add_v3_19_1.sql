1862e6c603a6235e8fbc76821c02403435487abe
haruto356 <harutoharu327@gmail.com>
2026-05-05 19:59:25 +0900

    add: v3.19.1のデータを追加

---
+++ migrations/0007_add_v3_19_1.sql
@@ -0,0 +1,7 @@
+INSERT INTO Composer (ComposerName) VALUES ("Bob Hou & Sherlock卓智媛 feat. 兰音Reine");
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("下一秒", 251, 150, 151, "3.19.1", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (302, 4, 8, 13, NULL, 155, 242, 500, NULL);
+
+INSERT INTO Composer (ComposerName) VALUES ("きくお");
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("玩具狂奏曲 -終焉-", 252, 130, 154, "3.19.1", 23);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (303, 8, 13, 15, 17, 489, 513, 1418, 1374);
