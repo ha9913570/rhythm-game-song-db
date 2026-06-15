513c684d723b516a221588243159f23377f1b003
haruto356 <harutoharu327@gmail.com>
2026-06-15 13:05:45 +0900

    update: バージョン3.19.2の曲の情報を追加

---
+++ migrations/0008_add_v3_19_2.sql
@@ -0,0 +1,7 @@
+INSERT INTO Composer (ComposerName) VALUES ("Nekock·LK");
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("亂★舞", 253, 0, 129, "3.19.2", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (304, 2, 7, 13, NULL, 119, 327, 548, NULL);
+
+INSERT INTO Composer (ComposerName) VALUES ("悠叶いのり");
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("Snow Dance", 254, 188, 118, "3.19.2", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAt) VALUES (305, 4, 8, 12, NULL, 170, 341, 558, NULL);
