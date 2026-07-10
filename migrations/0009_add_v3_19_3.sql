88fcea50fd4314603c5fbc65f8a64fd4c230b8e3
haruto356 <harutoharu327@gmail.com>
2026-07-11 08:20:05 +0900

    update: バージョン3.19.3の曲の情報を追加

---
+++ migrations/0009_add_v3_19_3.sql
@@ -0,0 +1,7 @@
+INSERT INTO Composer (ComposerName) VALUES ("Riya");
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("Cleyera", 255, 191, 136, "3.19.3", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (306, 5, 11, 15, NULL, 338, 822, 777, NULL);
+
+INSERT INTO Composer (ComposerName) VALUES ("かたぎり");
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("70 Minutes Fighters", 256, 223, 144, "3.19.3", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (307, 7, 12, 16, NULL, 365, 725, 1557, NULL);
