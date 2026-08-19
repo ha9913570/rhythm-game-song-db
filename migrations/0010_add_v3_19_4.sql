d1047ed31f0a665a825e40035ea0af921ed0325b
ha9913570 <harutoharu327@gmail.com>
2026-08-19 19:38:52 +0900

    add: バージョン3.19.4の曲を追加するSQLを追加

---
+++ migrations/0010_add_v3_19_4.sql
@@ -0,0 +1,8 @@
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("Gungnir Fracture", 91, 240, 149, "3.19.4", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (308, 6, 11, 16, NULL, 364, 506, 1185, NULL);
+
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("雪の降る日に", 146, 260, 132, "3.19.4", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (309, 2, 9, 16, NULL, 128, 455, 1243, NULL);
+
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("Der Schneid", 58, 222, 153, "3.19.4", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (310, 7, 11, 16, 17, 457, 710, 1426, 1555);
