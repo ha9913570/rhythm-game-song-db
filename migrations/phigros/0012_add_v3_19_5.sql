41df676ee78dec20cb9b20e97d2cdb15b0087404
ha9913570 <harutoharu327@gmail.com>
2026-09-17 08:20:16 +0900

    add: バージョン3. 19.5のデータを追加

---
+++ migrations/phigros/0012_add_v3_19_5.sql
@@ -0,0 +1,2 @@
+INSERT INTO Song (SongName, ComposerId, Bpm, SongLength, AddVersion, ChapterId) VALUES ("星拂云锦 feat. koi", 48, 0, 176, "3.19.5", 1);
+INSERT INTO Chart (SongId, DiffEZ, DiffHD, DiffIN, DiffAT, NoteEZ, NoteHD, NoteIN, NoteAT) VALUES (311, 4, 9, 15, NULL, 227, 600, 1235, NULL);
